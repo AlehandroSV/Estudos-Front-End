5154a8063c8dfd8613a5ba60b056b53b22a5afb5
AlehandroSV <alehandrosantos99@gmail.com>
2023-06-20 14:07:23 -0300

    feat; Revisando Diretivas

---
+++ Vue/Hora de Codar/Aulas/Aula 9/src/components/Info.vue
@@ -0,0 +1,26 @@
+<template>
+  <p v-if="esta_trabalhando">Estou trabalhando no momento.</p>
+  <p v-else>Estou em busca de novas oportunidades.</p>
+  <p>Utilizando as seguintes tecnologias.</p>
+  <ul>
+    <li>JavaScript</li>
+    <li>TypeScript</li>
+    <li>C#</li>
+    <li>Nest</li>
+    <li>Next</li>
+  </ul>
+  <p v-show="mostrar_email">Mande uma mensagem para o e-mail {{ email }}</p>
+</template>
+
+<script>
+export default {
+  name: "InfoPessoa",
+  data() {
+    return {
+      esta_trabalhando: false,
+      mostrar_email: true,
+      email: "alehandrosv.contato@gmail.com",
+    };
+  },
+};
+</script>
---
+++ Vue/Hora de Codar/Aulas/Aula 9/src/components/PrimeiroComponent.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>Olá Vue</h1>
+  <p>Meu nome é {{ nome }}</p>
+</template>
+
+<script>
+export default {
+  name: "PrimeiroComponent",
+  data() {
+    return {
+      nome: "Alehandro",
+    };
+  },
+};
+</script>
