5795da23e6f5564c33f5c4b8e1884998a4ebb757
AlehandroSV <alehandrosantos99@gmail.com>
2023-06-20 12:07:27 -0300

    feat: Component com SubComponent - Hierarquia

---
+++ Vue/Hora de Codar/Aulas/Aula 8/src/components/Info.vue
@@ -0,0 +1,20 @@
+<template>
+  <p>Estou trabalhando no momento.</p>
+  <p>Utilizando as seguintes tecnologias.</p>
+  <ul>
+    <li>JavaScript</li>
+    <li>TypeScript</li>
+    <li>C#</li>
+    <li>Nest</li>
+    <li>Next</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "InfoPessoa",
+  data() {
+    return {};
+  },
+};
+</script>
