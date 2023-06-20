c2da85a5d745fbd89249a8774366dbd76bbc5014
AlehandroSV <alehandrosantos99@gmail.com>
2023-06-20 11:01:43 -0300

    feat: Criando o primeiro Component

---
+++ Vue/Hora de Codar/Aulas/Aula 5/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Olá Mundo</h1>
+  <PrimeiroComponent />
+</template>
+
+<script>
+import PrimeiroComponent from "./components/PrimeiroComponent.vue";
+
+export default {
+  name: "App",
+  components: { PrimeiroComponent },
+};
+</script>
