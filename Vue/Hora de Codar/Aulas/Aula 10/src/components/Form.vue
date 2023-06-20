993ab9ae175f475a63f0c67f9d9ce5010f3c9dc8
AlehandroSV <alehandrosantos99@gmail.com>
2023-06-20 20:54:27 -0300

    feat: Implementando atributos dinamicos (v-bind)

---
+++ Vue/Hora de Codar/Aulas/Aula 10/src/components/Form.vue
@@ -0,0 +1,19 @@
+<template>
+  <h2>Meu formulário</h2>
+  <form action="">
+    <div><InputText /></div>
+    <div><InputText /></div>
+
+    <InputSubmit />
+  </form>
+</template>
+
+<script>
+import InputText from "./form/InputText.vue";
+import InputSubmit from "./form/Submit.vue";
+
+export default {
+  name: "FormVue",
+  components: { InputText, InputSubmit },
+};
+</script>
---
+++ Vue/Hora de Codar/Aulas/Aula 10/src/components/Info.vue
@@ -0,0 +1,35 @@
+<template>
+  <p v-if="esta_trabalhando">Estou trabalhando no momento.</p>
+  <p v-else>Estou em busca de novas oportunidades.</p>
+  <p>Utilizando as seguintes tecnologias.</p>
+  <ul>
+    <li>JavaScript</li>
+    <li>TypeScript</li>
+    <li>C#</li>
+    <li>Nest</li>
+    <li>Next</li>
+  </ul>
+  <p v-show="mostrar_email">Mande uma mensagem para o e-mail {{ email }}</p>
+  <p>
+    Para acessar meu portifólio basta
+    <a v-bind:href="meu_link" target="_blank">clicar aqui</a>
+  </p>
+  <Picture />
+</template>
+
+<script>
+import Picture from "./Picture.vue";
+
+export default {
+  name: "InfoPessoa",
+  data() {
+    return {
+      esta_trabalhando: false,
+      mostrar_email: true,
+      email: "alehandrosv.contato@gmail.com",
+      meu_link: "https://google.com",
+    };
+  },
+  components: { Picture },
+};
+</script>
