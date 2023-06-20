520e81c39dcac338bef5f008f884705ef23ffccc
AlehandroSV <alehandrosantos99@gmail.com>
2023-06-20 11:42:28 -0300

    feat: Aprendendo Life Cycle Hooks

---
+++ Vue/Hora de Codar/Aulas/Aula 7/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>{{ nome }}</h1>
+  <PrimeiroComponent />
+  <LifeCycle />
+</template>
+
+<script>
+import PrimeiroComponent from "./components/PrimeiroComponent.vue";
+import LifeCycle from "./components/LifeCycle.vue";
+
+export default {
+  name: "App",
+  components: { PrimeiroComponent, LifeCycle },
+  data() {
+    return {
+      nome: "Essa é minha APP",
+    };
+  },
+};
+</script>
---
+++ Vue/Hora de Codar/Aulas/Aula 7/src/components/LifeCycle.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>Meu nome é: {{ nome }}</h1>
+</template>
+
+<script>
+export default {
+  name: "LifeCyCle",
+  data() {
+    return {
+      nome: "ainda não sei",
+    };
+  },
+  created() {
+    setTimeout(() => {
+      this.nome = "Alehandro";
+    }, 2000);
+  },
+  mounted() {
+    setTimeout(() => {
+      this.nome = "João";
+    }, 3000);
+  },
+};
+</script>
