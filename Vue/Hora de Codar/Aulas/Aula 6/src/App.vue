badfa9f55c255d8ba1c2e5ae323504aee0d7616c
AlehandroSV <alehandrosantos99@gmail.com>
2023-06-20 11:30:24 -0300

    feat: Utilizando Dados em components

---
+++ Vue/Hora de Codar/Aulas/Aula 6/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>Olá Mundo</h1>
+  <PrimeiroComponent />
+  {{ teste }}
+</template>
+
+<script>
+import PrimeiroComponent from "./components/PrimeiroComponent.vue";
+
+export default {
+  name: "App",
+  components: { PrimeiroComponent },
+  data() {
+    return {
+      teste: "Testes",
+    };
+  },
+};
+</script>
