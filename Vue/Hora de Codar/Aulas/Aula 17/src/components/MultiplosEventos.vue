b8319055f750d7b348ac50581c0b02784d8781fb
AlehandroSV <alehandrosantos99@gmail.com>
2023-06-22 10:52:21 -0300

    feat: Utilizando Props

---
+++ Vue/Hora de Codar/Aulas/Aula 17/src/components/MultiplosEventos.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <button
+      @click="
+        primeiro('Primeiro evento'), segundo('Segundo evento'), terceiro()
+      "
+    >
+      Ativar múltiplos eventos
+    </button>
+    <p>{{ multiplos_eventos }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MultiplosEventos",
+  data() {
+    return {
+      multiplos_eventos: "",
+    };
+  },
+  methods: {
+    primeiro(text) {
+      this.multiplos_eventos = text;
+    },
+    segundo(text) {
+      setTimeout(() => {
+        this.multiplos_eventos = text;
+      }, 1000);
+    },
+    terceiro() {
+      setTimeout(() => {
+        this.multiplos_eventos = "";
+      }, 2000);
+    },
+  },
+};
+</script>
---
+++ Vue/Hora de Codar/Aulas/Aula 17/src/components/Pessoa.vue
@@ -0,0 +1,22 @@
+<template>
+  <h2>Essa é a descrição da pessoa: {{ nome }}</h2>
+  <Form />
+  <Info :email="email" :estaTrabalhando="estaTrabalhando" />
+</template>
+
+<script>
+import Info from "./Info.vue";
+import Form from "./Form.vue";
+
+export default {
+  name: "PessoaVue",
+  data() {
+    return {
+      nome: "Alehandro",
+      email: "alehandrosv.contato@gmail.com",
+      estaTrabalhando: true,
+    };
+  },
+  components: { Info, Form },
+};
+</script>
