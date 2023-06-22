f0cff88b1848a4d29427647e7083c1660258eb8e
AlehandroSV <alehandrosantos99@gmail.com>
2023-06-22 10:25:48 -0300

    feat: Implementando multiplos eventos

---
+++ Vue/Hora de Codar/Aulas/Aula 15/src/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+  <ul>
+    <li><a href="#">Home</a></li>
+    <li><a href="#">Projeto</a></li>
+    <li><a href="#">Contato</a></li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "HeaderItem",
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  display: flex;
+  gap: 1rem;
+}
+
+li {
+  margin-right: 0;
+}
+
+a:hover {
+  color: crimson;
+  transition: all 1000ms;
+}
+
+a {
+  color: aquamarine;
+}
+</style>
---
+++ Vue/Hora de Codar/Aulas/Aula 15/src/components/PrimeiroComponent.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>Olá Vue</h1>
+  <p>Meu nome é {{ nome }}</p>
+  <MultiplosEventos />
+</template>
+
+<script>
+import MultiplosEventos from "./MultiplosEventos.vue";
+export default {
+  components: { MultiplosEventos },
+  name: "PrimeiroComponent",
+  data() {
+    return {
+      nome: "Alehandro",
+    };
+  },
+};
+</script>
