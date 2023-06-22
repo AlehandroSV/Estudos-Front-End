56d0c64d2f273bb7cca6414d194bcd95d556f4d6
AlehandroSV <alehandrosantos99@gmail.com>
2023-06-22 10:38:15 -0300

    feat: Reutilização de Components

---
+++ Vue/Hora de Codar/Aulas/Aula 16/src/components/Info.vue
@@ -0,0 +1,73 @@
+<template>
+  <p v-if="esta_trabalhando">Estou trabalhando no momento.</p>
+  <p v-else>Estou em busca de novas oportunidades.</p>
+  <p>Utilizando as seguintes tecnologias. (Front-End)</p>
+  <ul>
+    <li v-for="(tecnologias, index) in front_end" :key="index">
+      {{ tecnologias }}
+    </li>
+  </ul>
+
+  <p>Utilizando as seguintes tecnologias. (Back-End)</p>
+  <ul>
+    <li v-for="tecnologias in back_end" :key="tecnologias.id">
+      {{ tecnologias.name }}
+    </li>
+  </ul>
+  <div>
+    <button @click="showEmail">{{ texto_button_email }}</button>
+  </div>
+  <p v-show="mostrar_email">Mande uma mensagem para o e-mail {{ email }}</p>
+  <p>
+    Para acessar meu portifólio basta
+    <a v-bind:href="meu_link" target="_blank">clicar aqui</a>
+  </p>
+  <Picture />
+</template>
+
+<script>
+import Picture from "./Picture.vue";
+
+export default {
+  name: "InfoPessoa",
+  data() {
+    return {
+      esta_trabalhando: false,
+      mostrar_email: false,
+      email: "alehandrosv.contato@gmail.com",
+      meu_link: "https://google.com",
+      texto_button_email: "Mostrar E-Mail",
+      back_end: [
+        { id: 1, name: "JavaScript" },
+        { id: 2, name: "TypeScript" },
+        { id: 3, name: "C#" },
+        { id: 4, name: ".NET" },
+        { id: 5, name: "Nest" },
+      ],
+      front_end: [
+        "JavaScript",
+        "TypeScript",
+        "C#",
+        "Next",
+        "React",
+        "React Native",
+        "Vue",
+        "Nuxt",
+        "Etc...",
+      ],
+    };
+  },
+  components: { Picture },
+  methods: {
+    showEmail() {
+      this.mostrar_email = !this.mostrar_email;
+
+      if (!this.mostrar_email) {
+        this.texto_button_email = "Mostrar E-Mail";
+      } else {
+        this.texto_button_email = "Esconder E-Mail";
+      }
+    },
+  },
+};
+</script>
---
+++ Vue/Hora de Codar/Aulas/Aula 16/src/components/PrimeiroComponent.vue
@@ -0,0 +1,26 @@
+<template>
+  <h1>Olá Vue</h1>
+  <p>Meu nome é {{ nome }}</p>
+  <MultiplosEventos />
+  <Reutilizacao />
+  <Reutilizacao />
+  <Reutilizacao />
+  <Reutilizacao />
+  <Reutilizacao />
+  <Reutilizacao />
+  <Reutilizacao />
+</template>
+
+<script>
+import MultiplosEventos from "./MultiplosEventos.vue";
+import Reutilizacao from "./Reutilizacao.vue";
+export default {
+  components: { MultiplosEventos, Reutilizacao },
+  name: "PrimeiroComponent",
+  data() {
+    return {
+      nome: "Alehandro",
+    };
+  },
+};
+</script>
