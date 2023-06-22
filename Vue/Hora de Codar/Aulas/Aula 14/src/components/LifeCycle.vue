55a0faa5ee2cfb26c7737ffd3c11ccfa5d66ef98
AlehandroSV <alehandrosantos99@gmail.com>
2023-06-22 10:16:03 -0300

    feat: Eventos (@click e @submit)

---
+++ Vue/Hora de Codar/Aulas/Aula 14/src/components/LifeCycle.vue
@@ -0,0 +1,30 @@
+<template>
+  <h1>Meu nome é: {{ nome }}</h1>
+</template>
+
+<script>
+export default {
+  name: "LifeCyCle",
+  data() {
+    return {
+      nome: "ainda não sei",
+    };
+  },
+  created() {
+    this.lifeCycle();
+    setTimeout(() => {
+      this.nome = "Alehandro";
+    }, 2000);
+  },
+  mounted() {
+    setTimeout(() => {
+      this.nome = "João";
+    }, 3000);
+  },
+  methods: {
+    lifeCycle() {
+      console.log("Executou");
+    },
+  },
+};
+</script>
