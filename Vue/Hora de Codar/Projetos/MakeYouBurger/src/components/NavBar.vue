28179f152e281b8e0c930b46cbf58131d13ddc9f
AlehandroSV <alehandrosantos99@gmail.com>
2023-06-22 12:00:05 -0300

    feat: Implementando components Globais e Ajustando Rotas

---
+++ Vue/Hora de Codar/Projetos/MakeYouBurger/src/components/NavBar.vue
@@ -0,0 +1,15 @@
+<template>
+  <header id="nav">
+    <router-link to="/"><img src="" alt="" /></router-link>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/pedidos">Pedidos</router-link>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "NavBar",
+};
+</script>
+
+<style scoped></style>
